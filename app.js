84968bc35c39003d01ca7b72dea72abc0c88a645
Jordan Wright <jaws610@gmail.com>
2022-06-03 02:00:10 -0400

    Working with Web Wokers I have been able to get two indepent counter to work on the same page, at the same time. This is the basis for having countdown timers for my build orders.

--- app.js
+++ app.js
@@ -1,16 +1,35 @@
 const myWorker = new Worker('worker.js');
 
 myWorker.onmessage = function(message){
-    console.log('Message Recieved From Worker');
-    console.log(message);
+    console.log('got message');
+    document.getElementById('counterOne').innerHTML = message['data'];
 }
 
 const workerButton = document.getElementById('worker');
 const otherButton = document.getElementById('notWorker');
 
 workerButton.addEventListener('click', sendMessage);
-otherButton.addEventListener('click', () => console.log('Hello, Jordan'));
+// otherButton.addEventListener('click', () => console.log('Hello, Jordan'));
 
+
+let testFunction = function(){
+    
+    let counter = 1;
+
+    let myInterval = setInterval(function(){
+        console.log(counter);
+        document.getElementById('counterTwo').innerHTML = counter
+        counter += 1
+        
+        if (counter > 10){
+            clearInterval(myInterval);
+        }
+     }, 1000);
+ 
+ }
+
+
+otherButton.addEventListener('click', testFunction);
 
 function sendMessage(){
     myWorker.postMessage('Hello World');
--- worker.js
+++ worker.js
@@ -3,9 +3,26 @@
 // }
 
 self.onmessage = function(message){
-    console.log(message);
-    console.log(message['data']);
 
-    self.postMessage('You suck');
+    let counter = 1;
+
+   let myInterval = setInterval(function(){
+        console.log(counter);
+
+        self.sendMessage(counter);
+        counter += 1
+
+        if (counter > 10){
+            clearInterval(myInterval);
+        }
+    }, 1000);
+
 }
+
+sendMessage = function(count){
+    self.postMessage(count)
+}
+
+
+
 
