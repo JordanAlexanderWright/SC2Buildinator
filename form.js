62312cd05c76b87d4ccdceffdf066cf0a6ce687f
Jordan Wright <jaws610@gmail.com>
2022-06-23 12:49:50 -0400

    Refactoring Input Handling
    Created a function that prevents me from writing repeating code for all
    of the forms I want to add UX changes to. Still needs some work,
    would like to create a list of the elements, but this is fairly
    compact for now.

--- form.js
+++ form.js
@@ -10,51 +10,30 @@ document.getElementById('objectSelector').addEventListener(`keydown`, function(e
 
 // Checks for length in input and prevets if it is over 2 (two digits of minute / seconds). Also prevents decmial points
 
-document.getElementById('minuteSelector').addEventListener(`keydown`, function(e){
-    if ((e.target.value.length >= 2) && (e.code != 'Backspace')){
-        e.preventDefault();
-    } if ((e.code.includes('Digit') === false) && (e.code.includes('Backspace') === false)){
-        console.log('prevent')
-        e.preventDefault();
-    }     
-})
+function userInputExperience (field, fieldLength, maxValue){
 
-// Validates minute field, making sure it is < 60
-document.getElementById('minuteSelector').addEventListener(`keyup`, function(e){
-    if (parseInt(e.target.value) > 59){
-        e.target.value = '';
-        console.log('Invalid Minutes Input, Please Try Again');
-    }
-})
+    // This makes sure the input is a digit (excluding backspace) and that the length of the field
+    // Is staying under the maximum length the field should be.
 
-document.getElementById('secondSelector').addEventListener(`keydown`,  function(e){
-    if ((e.target.value.length >= 2) && (e.code != 'Backspace')){
-        e.preventDefault();
-    } if ((e.code.includes('Digit') === false) && (e.code.includes('Backspace') === false)){
-        console.log('prevent')
-        e.preventDefault();
-    }     
-})
+    field.addEventListener(`keydown`, function(e){ 
 
-document.getElementById('secondSelector').addEventListener(`keyup`, function(e){
-    if (parseInt(e.target.value) > 59){
-        e.target.value = '';
-        console.log('Invalid Seconds Input, Please Try Again');
-    }
-})
+        if ((e.target.value.length >= fieldLength) && (e.code != 'Backspace')){
+            e.preventDefault();
+        } if ((e.code.includes('Digit') === false) && (e.code.includes('Backspace') === false)){
+            console.log('prevent')
+            e.preventDefault();
+        }})
+    // This is validating that the field hasn't gone above the maximum value the field should be. 
+    // Has to be placed on 'keyup' or could be grouped in previous EventListener
 
-document.getElementById('supplySelector').addEventListener(`keydown`,  function(e){
-    if ((e.target.value.length >= 3) && (e.code != 'Backspace')){
-        e.preventDefault();
-    } if ((e.code.includes('Digit') === false) && (e.code.includes('Backspace') === false)){
-        console.log('prevent')
-        e.preventDefault();
-    }     
-})
+    field.addEventListener(`keyup`, function(e){
+        if (parseInt(e.target.value) > maxValue){
+            e.target.value = '';
+            console.log('Invalid Minutes Input, Please Try Again');
+        }
+    })
+}
 
-document.getElementById('supplySelector').addEventListener(`keyup`, function(e){
-    if (parseInt(e.target.value) > 200){
-        e.target.value = '';
-        console.log('Invalid Supply Input, Please Try Again');
-    }
-})
+userInputExperience(document.getElementById('minuteSelector'), 2, 59);
+userInputExperience(document.getElementById('secondSelector'), 2, 59);
+userInputExperience(document.getElementById('supplySelector'), 3, 200);
